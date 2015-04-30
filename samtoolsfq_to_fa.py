79eff8001e1618a5841a16ba0162387ad694230d
Emily Jane McTavish <ejmctavish@gmail.com>
2015-04-30 16:11:05 +0200

    helper script to convert files

---
+++ samtoolsfq_to_fa.py
@@ -0,0 +1,25 @@
+#!usr/bin/python
+
+import sys
+
+fq=sys.argv[1]
+fa=sys.argv[2]
+query=sys.argv[3]
+
+fql=open(fq).readlines()
+
+
+assert fql[0].startswith('@')
+
+fal=open(fa,'w')
+for lin in fql:
+	if lin.startswith('+'):
+		break
+	if lin.startswith('@'):
+		fal.write('>{}\n'.format(query))
+	else:
+		fal.write(lin)
+
+fal.close
+
+

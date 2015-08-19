7883176b4939e0da4ba572852d9cd7ee07ce0f4a
Emily Jane McTavish <ejmctavish@gmail.com>
2015-08-19 17:49:34 +0200

    finds seqs, places

--- genetree-updating/generate_wanted_ids.py
+++ genetree-updating/generate_wanted_ids.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 import sys
-from dendropy import Tree
+from dendropy import Tree, DnaCharacterMatrix
 from peyotl import gen_otu_dict, iter_node
 from peyotl.manip import iter_trees
 from peyotl.phylesystem.phylesystem_umbrella import Phylesystem
@@ -12,41 +12,39 @@ import re
 import subprocess
 import time
 
-E_VALUE_THRESH = 0.04
-
-Entrez.email = "ejmctavish@gmail.com"
-
-
-study_id=sys.argv[1]
-tree_id=sys.argv[2]
-seqmat=sys.argv[3]
-mattype=sys.argv[4]
-
-ott_ncbi="../ott_ncbi"
-study_id = "pg_2739"
-tree_id = "tree6601"
-seqmat = "tree6601.fas"
-mattype = "fasta"
-print study_id
-
+'''
 #LSU ASC tree
 ott_ncbi="../ott_ncbi"
 study_id = "pg_873"
 tree_id = "tree1679"
 #seqmat = "ASC_LSU.nex"
-seqmat = "ASC_LSU_nogap.fas"
+seqaln = "mat1.fas"
 #mattype = "nexus"
+runname="asc_test"
 mattype="fasta"
 print study_id
+'''
+
+study_id=sys.argv[1]
+tree_id=sys.argv[2]
+seqaln=sys.argv[3]
+mattype=sys.argv[4]
+runname=sys.argv[5]
+
+
+#Fixed values
+E_VALUE_THRESH = 0.04
+ott_ncbi="../ott_ncbi" #TODO config file
+Entrez.email = "ejmctavish@gmail.com"
+
+
 
 phy = Phylesystem()
-
-
 n = phy.return_study(study_id)[0]
+api_wrapper.study.get(study_id,tree=tree_id)
 
-#There has gort to be a better way to get the ingroup otus...
+##This is a weird way to get the ingroup node, but I need the OTT ids anyhow.
 m = extract_tree(n, tree_id, PhyloSchema('newick', output_nexml2json = '1.2.1', content="tree", tip_label="ot:ottId"), subtree_id="ingroup")
-
 otu_dict = gen_otu_dict(n)
 ottids = []
 for oid, o in otu_dict.items():
@@ -62,11 +60,51 @@ for oid, o in otu_dict.items():
 
 mrca_node = tree_of_life.mrca(ott_ids=ottids, wrap_response=True)
 
-#curl -X POST http://api.opentreeoflife.org/v2/taxonomy/taxon -H "content-type:application/json" -d '{"ott_id":225495, "list_terminal_descendants": "True"}'
+newick = extract_tree(n, tree_id, PhyloSchema('newick', output_nexml2json = '1.2.1', content="tree", tip_label="ot:originalLabel"))
+newick = newick.replace(" ", "_") #UGH
 
-#named_node = tree_of_life.mrca(ott_ids=[mrca_node.nearest_taxon.ott_id], wrap_response=True)
-#newick = named_node.subtree_newick # NOTE: Excludes daughter taxa that  are considered non monophylo.
+d = DnaCharacterMatrix.get(path=seqaln,
+                           schema=mattype)
+# make the taxon_namespace immutable, so the tree does not add
+#   new labels...
+d.taxon_namespace.is_mutable = True
+tre = Tree.get(data=newick,
+                schema="newick",
+                taxon_namespace=d.taxon_namespace)
 
+# get all of the taxa associated with tips of the tree, and make sure that
+#   they include all of the members of the data's taxon_namespace...
+treed_taxa = [i.taxon for i in tre.leaf_nodes()]
+if len(treed_taxa) != len(d.taxon_namespace):
+    missing = [i.label for i in d.taxon_namespace if i not in treed_taxa]
+    emf = 'Some of the taxa in the alignment are not in the tree. Missing "{}"\n'
+    em = emf.format('", "'.join(missing))
+    raise ValueError(em)
+
+elem_ord = n['nexml']['^ot:otusElementOrder'][0]
+
+map_dict = {}
+idsset =set()
+for otu in n['nexml']['otusById'][elem_ord]['otuById']:
+    orig = n['nexml']['otusById'][elem_ord]['otuById'][otu]['^ot:originalLabel']
+    try:
+        ott_id = n['nexml']['otusById'][elem_ord]['otuById'][otu]['^ot:ottId']
+        if ott_id in idsset:
+            ott_id=orig.replace(" ","_")
+        idsset.add(ott_id)
+    except:
+        ott_id=orig.replace(" ","_")
+    map_dict[orig.replace(" ","_")]=str(ott_id)
+
+for taxon in d.taxon_namespace:
+    taxon.label = map_dict[taxon.label]
+
+
+tre.resolve_polytomies()
+tre.write(path = "{}_random_resolve.tre".format(runname), schema = "newick", unquoted_underscores=True, suppress_edge_lengths=True)
+
+d.write(path="{}_aln_ott.phy".format(runname), schema="phylip")
+d.write(path="{}_aln_ott.fas".format(runname), schema="fasta")
 
 ott_to_ncbi = {}
 ncbi_to_ott = {}
@@ -79,44 +117,23 @@ for lin in fi:
 
 fi.close()
 
-'''matches = re.finditer('ott(\d*)', newick) 
-wanted_ncbi_ids = []
-unfound = []
-for item in matches:
-    try:
-        wanted_ncbi_ids.append(ott_to_ncbi[item.group(1)]
-    except:
-        unfound.append(item.group(1))
-'''
-
-#NOW: work through the alignment, blast each seq (EACH?)
-#and make a list of genbacnk id's that match.
-
-#OR ancestral states?
-#blastn -remote -query fulltest3/cns.fa -db nr -max_target_seqs 1 -outfmt "6 qseqid staxids"
-'''fi = open("/home/ejmctavish/ncbi/gi_taxid_nucl.dmp")
-gi_taxid={}
-for lin in fi:
-    gi_taxid[lin.split()]
-'''
- bsave= 
 
 equery = "txid{}[orgn]".format(ott_to_ncbi[mrca_node.nearest_taxon.ott_id])
 
 for i, record in enumerate(SeqIO.parse(seqmat, mattype)):
     result_handle = NCBIWWW.qblast("blastn", "nt", record.format("fasta"),  entrez_query=equery)
-    save_file = open("asc_blast_{}.xml".format(i), "w")
+    save_file = open("{}_{}.xml".format(runname,i), "w")
     save_file.write(result_handle.read())
     save_file.close()
     result_handle.close()
 
 
+
 gi_to_ncbi = {}
 new_seqs={}
 
-#for i, record in enumerate(SeqIO.parse(seqmat, mattype)):
-for i in range(26):
-    result_handle = open("asc_blast_{}.xml".format(i))
+for i, record in enumerate(SeqIO.parse(seqmat, mattype)):
+    result_handle = open("{}_{}.xml".format(runname,i))
     blast_records = NCBIXML.parse(result_handle)
     for blast_record in blast_records:
         for alignment in blast_record.alignments:
@@ -126,16 +143,6 @@ for i in range(26):
                    gi_to_ncbi[int(alignment.title.split('|')[1])] = ''
 ##SET MINIMUM SEQUENCE LENGTH
 
-
-
- #This part is sort of crazy slooow
-'''fi = open("/home/ejmctavish/ncbi/gi_taxid_nucl.dmp")
-for lin in fi:
-    if int(lin.split()[0]) in gi_to_ncbi.keys():
-        lii=lin.split()
-        gi_to_ncbi[int(lii[0])] = int(lii[1])
-
-fi.close()'''
 
 for gi in gi_to_ncbi.keys():
     if gi_to_ncbi[gi] == '':
@@ -151,7 +158,7 @@ for gi in gi_to_ncbi.keys():
             pass
 
 
-newseqs = "newseqs_asc.fasta"
+newseqs = "{}.fasta".format(runname)
 fi = open(newseqs,'w')
 for gi in gi_to_ncbi:
         try:
@@ -176,66 +183,26 @@ fi.close()
 #and a RESOLVED TREE?! w/ BL?!
 
 
-subprocess.popen("papara", "-t", tree, "-s" alignment, "-q",  newseqs, "-n", "extended") 
+subprocess.Popen(["papara", "-t","{}_random_resolve.tre".format(runname), "-s", "{}_aln_ott.phy".format(runname), "-q",  newseqs, "-n", "extended"]) 
                           #run RAXML EPA on the alignments
 
 #placement
-raxmlHPC -m GTRCAT -f v -s papara_alignment.multi_consensus -t ${WD}/$tree -n ${nam}_PLACE
+subprocess.Popen(["raxmlHPC", "-m", "GTRCAT", "-f", "v", "-s", "papara_alignment.extended", "-t","{}_random_resolve.tre".format(runname), "-n", "{}_PLACE".format(runname)])
 
 
+placetre = Tree.get(path="RAxML_labelledTree.{}_PLACE".format(runname),
+                schema="newick")
+
+for taxon in placetre.taxon_namespace:
+    if taxon.label.startswith("QUERY"):
+        taxon.label=taxon.label.split()[1]
+    taxon.label=taxon.label.replace(" ", "_") #Look for real dendropy way!!
+
+
+#NEEDS ASSERT THAT NAME SPACES ARE SAME!
+
+placetre.resolve_polytomies()
+placetre.write(path = "{}_place_resolve.tre".format(runname), schema = "newick", unquoted_underscores=True)
 #Full run with starting tree from placements
-raxmlHPC -m GTRCAT -s papara_alignment.multi_consensus -t ${WD}/$tree -n update
 
-
-
-'''
-
-
-
-info = taxonomy.taxon(ott_id= mrca_node.nearest_taxon.ott_id,
-                          include_lineage=False,
-                          list_terminal_descendants=True,
-                          wrap_response=False)
-named_node = tree_of_life.mrca(ott_ids=[mrca_node.nearest_taxon.ott_id], wrap_response=True)
-newick = named_node.subtree_newick
-
-names = re.split(r'[;,\s]\s*', newick)
-names = newick.replace('(',',').replace(')',',').split(",")
-
-
-
-
-
-
-
-
-
-
-
-
-
-DOMAIN = 'https://tree.opentreeoflife.org'
-URL_PATH_FMT ='opentree/argus/otol.draft.22@{i:d}'
-URL_FMT = DOMAIN + '/' + URL_PATH_FMT
-
-url = URL_FMT.format(i=mrca_node.node_id)        
-if mrca_node.is_taxon:
-        errstream.write('The node in the Graph of Life corresponds to a taxon:\n')
-        mrca_node.write_report(errstream)
-else:
-    errstream.write('The node in the Graph of Life does not correspond to a taxon.\nThe most recent ancestor which is also a named taxon in OTT is:\n')
-    mrca_node.nearest_taxon.write_report(errstream)
-if subtree:
-        # We could ask for this using: 
-        #   newick = tree_of_life.subtree(node_id=mrca_node.node_id)['newick']
-        # or we can ask the GoLNodeWrapper object to do the call (as shown below)
-        try:
-            newick = mrca_node.subtree_newick
-        except Exception as x:
-            errstream.write('Could not fetch the subtree. Error: {}\n'.format(str(x)))
-        else:
-            errstream.write('The newick representation of the subtree rooted at this node is:\n')
-            output.write('{}\n'.format(newick))
-
-induced_newick = tree_of_life.induced_subtree(ott_ids=id_list)['subtree']
-'''
+subprocess.Popen(["raxmlHPC", "-m", "GTRCAT", "-s", "papara_alignment.extended", "-t","{}_place_resolve.tre".format(runname), "-p", "1", "-n", "{}".format(runname)])

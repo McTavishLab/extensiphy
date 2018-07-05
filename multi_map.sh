5c8d8dcc3b7bdc7c59030dd2beeb5faa52b0130c
jtfield <jtoscanifield@ucmerced.edu>
2018-07-05 14:22:09 -0700

    added wrapper program that will be used for multiple new species adds

---
+++ multi_map.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+PHYCORDER=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+ALIGN=$1
+
+TREE=$2
+
+READ_DIR=$3
+
+OUTPUT=$4
+
+cd $READ_DIR
+READ_LOC=$(pwd)
+for i in $(ls *R1_.fastq); do
+    "$PHYCORDER/map_to_align.sh -a $ALIGN -t $TREE -p "$READ_loc/$i" -e "{$i%R1_.fastq}R2_.fastq" -o $OUTPUT > "$OUTPUT/multi_map_dev.log"
+done

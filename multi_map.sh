281aa1383051cf915e92534e87076383bce3e16e
jtfield <jtoscanifield@ucmerced.edu>
2018-07-05 16:56:53 -0700

    added final concat for multiple map_to_align runs before RAxML

--- multi_map.sh
+++ multi_map.sh
@@ -13,5 +13,13 @@ OUTPUT=$4
 cd $READ_DIR
 READ_LOC=$(pwd)
 for i in $(ls *R1_.fastq); do
-    "$PHYCORDER/map_to_align.sh -a $ALIGN -t $TREE -p "$READ_loc/$i" -e "{$i%R1_.fastq}R2_.fastq" -o $OUTPUT > "$PHYCORDER/multi_map_dev.log"
+    "$PHYCORDER/map_to_align.sh -a $ALIGN -t $TREE -p "$READ_LOC/$i" -e "$READ_LOC{$i%R1_.fastq}R2_.fastq" -o $OUTPUT > "$PHYCORDER/multi_map_dev.log"
 done
+
+wait
+
+cd $OUTPUT
+
+cat *aligned_cns.fas > $OUTPUT/extended.aln
+
+raxmlHPC-PTHREADS-SSE3 -m GTRGAMMA -s extended.aln -t $TREE -p 12345 -n consensusFULL

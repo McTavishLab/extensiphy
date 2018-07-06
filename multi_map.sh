ebf510eea7a2b6c2cd0960848e1f4da2324a588e
jtfield <jtoscanifield@ucmerced.edu>
2018-07-06 11:14:19 -0700

    added opts to pipeline to fix functionality

--- multi_map.sh
+++ multi_map.sh
@@ -2,26 +2,89 @@
 
 PHYCORDER=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-ALIGN=$1
+while getopts ":a:t:p:o:n:r:m:b:w:t:h" opt; do
+  case $opt in
+    a) align="$OPTARG"
+    ;;
+    t) tree="$OPTARG"
+    ;;
+    p) read_dir
+    ;;
+    o) outdir="$OPTARG"
+	;;
+	n) nam="$OPTARG"
+    ;;
+    r) read_align="$OPTARG"
+    ;;
+    m) map="$OPTARG"
+    ;;
+    b) re_map="$OPTARG"
+    ;;
+    w) wre_map="$OPTARG"
+    ;;
+    t) threads="$OPTARG"
+    ;;
+    h) echo  "alignment in fasta format (-a), tree in Newick format (-t), and reads in fastq (-p -e paired_end_base_filenames or -s single_end_base_filename required)"
+    exit
+    ;;
+    \?) echo "Invalid option -$OPTARG" >&2
+    ;;
+  esac
+done
 
-TREE=$2
+if [ -z "$align" ] || [ -z "$tree" ]; then
+   "alignment (-a), tree (-t), and reads (-p or -s required)"
+   exit
+fi
 
-READ_DIR=$3
+#Ttest if files actually exist
+#Check to make sure mapping has occured if re-mapping
 
-OUTPUT=$4
+if [ -f "$align" ]; then
+    printf "Alignment is %s\n" "$align"
+  else
+    printf "Alignment $align not found. Exiting\n" >&2
+    exit
+fi
+if [ -f "$tree" ]; then
+    printf "Tree is %s\n" "$tree"
+  else
+    printf "Tree $tree not found. Exiting\n" >&2
+    exit
+fi
+if [ $PE -eq 1 ]; then
+  if [ -f ${read_one} ]; then
+     printf "Paired end reads \n"
+     printf "read one is ${read_one}\n"
+  else
+    printf "read one ${read_one} not found. Exiting\n" >&2
+    exit
+fi
+  if [ -f ${read_two} ]; then
+     printf "Paired end reads \n"
+     printf "read two is ${read_two}\n"
+  else
+    printf "read two ${read_two} not found. Exiting\n" >&2
+    exit
+fi
+fi
+printf "Argument out is %s\n" "$outdir"
+printf "Argument name is %s\n" "$nam"
+printf "Argument map is %s\n" "$map"
+printf "Argument re_mapis %s\n" "$re_map"
 
-THREADS=$5
+mkdir -p $outdir
 
-cd $READ_DIR
-READ_LOC=$(pwd)
+#cd $READ_DIR
+#READ_LOC=$(pwd)
 for i in $(ls *R1_.fastq); do
-    $PHYCORDER/map_to_align.sh -a $ALIGN -t $TREE -p "$READ_LOC/$i" -e "$READ_LOC{$i%R1_.fastq}R2_.fastq" -t $THREADS -o $OUTPUT > "$PHYCORDER/multi_map_dev.log"
+    $PHYCORDER/map_to_align.sh -a $align -t $tree -p "$read_dir/$i" -e "$read_dir{$i%R1_.fastq}R2_.fastq" -t $threads -o $outdir > "$PHYCORDER/multi_map_dev.log"
 done
 
 wait
 
-cd $OUTPUT
+cd $outdir
 
-cat *aligned_cns.fas > $OUTPUT/extended.aln
+cat *aligned_cns.fas > $outdir/extended.aln
 
-raxmlHPC-PTHREADS-SSE3 -m GTRGAMMA -T $THREADS -s extended.aln -t $TREE -p 12345 -n consensusFULL
+raxmlHPC-PTHREADS-SSE3 -m GTRGAMMA -T $threads -s extended.aln -t $tree -p 12345 -n consensusFULL

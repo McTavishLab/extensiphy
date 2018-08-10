4fc72cfba965c5935fbcda6389687234acfbaac5
jtfield <jtoscanifield@ucmerced.edu>
2018-08-10 15:09:14 -0700

    updated #files>core handling section

--- multi_map.sh
+++ multi_map.sh
@@ -104,13 +104,32 @@ then
 
   printf "Multiple taxa update of phylogenetic tree complete\n"
 else
-  counter=0
-  while [ $counter -lt $threads]; do
+  # section handles times when you are adding more tips to the tree than available processors
+  echo "Number of taxa being added to alignment and tree are greater than number of processors\n"
+  echo "Beginning job-number controlled Phycorder run\n"
+
+  for i in $(ls *R1_.fastq); do
     time $PHYCORDER/map_to_align.sh -a $align -t $tree -p "$read_dir"/"$i" -e "$read_dir"/"${i%R1_.fastq}R2_.fastq" -c $threads -o "$i"_"output_dir" > "$PHYCORDER/multi_map_dev.log" &
     printf "adding new map_to_align run\n"
+    while [ $(jobs | wc -l) -ge $threads ] ; do sleep 1 ; done
   done
 
   wait
 
-  # NEED SECTION ON HOW TO IDENTIFY FILES THAT HAVE BEEN PROCESSED ALREADY AND IGNORE THEM
-  
+  printf "Individual Phycorder runs finished. Combining aligned query sequences and adding them to starting alignment\n"
+
+  mkdir combine_and_infer
+
+  for i in $(ls -d *_output_dir); do
+    cp $i/*R1*.fas ./
+  done
+
+  cp $align $read_dir
+
+  cat *.fas > extended.aln
+
+  printf "Extended alignment file creaded (extended.aln), using previous tree as starting tree for phylogenetic inference\n"
+
+  raxmlHPC-PTHREADS-AVX -m GTRGAMMA -T $threads -s extended.aln -t $tree -p 12345 -n consensusFULL
+
+  printf "Multiple taxa update of phylogenetic tree complete\n"

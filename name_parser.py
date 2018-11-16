43859c669d4cfcce8065360052bdbd0adf2cf2a6
vortacs <jtoscanifield@ucmerced.edu>
2018-11-16 14:00:07 -0800

    Renames taxa in alignment, writes new alignment fasta file and OTU dict

--- name_parser.py
+++ name_parser.py
@@ -11,16 +11,31 @@ readdata = data.read()
 strp = readdata.strip()
 splt = strp.split('>')
 
+clean_splt = splt[1:]
+
 newnames_oldnames = {}
 OTU_count = 0
 
-for i in splt:
+for i in clean_splt:
     count = 0
     OTU_count+=1
     j = i.split('\n')
     for segment in j:
         count+=1
-        if count==1 and len(segment)>2:
+        if count==1:
             newnames_oldnames['OTU_' + str(OTU_count)] = segment
 
-print(newnames_oldnames)
+OTU_name_dict = "OTU_name_dict.txt"
+new_alignment = open("renamed_alignment.fa", 'w')
+
+for key, value in newnames_oldnames.iteritems():
+    for item in clean_splt:
+        if re.match(value, item):
+            replaces = re.sub(value, key, item)
+            new_alignment.write(">")
+            new_alignment.write(replaces)
+            new_alignment.write('\n')
+
+with open(OTU_name_dict, 'w') as f:
+    for key, value in newnames_oldnames.items():
+        f.write('%s:%s\n' % (key, value))

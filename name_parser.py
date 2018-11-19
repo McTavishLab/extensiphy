fabe4e0067f3720f20596bad8e4993efd40c0ef1
vortacs <jtoscanifield@ucmerced.edu>
2018-11-19 13:01:24 -0800

    writing out new dictionary of names and OTU keys

--- name_parser.py
+++ name_parser.py
@@ -50,6 +50,7 @@ def main():
         current_OTU_count = 0
         OTU_nums = []
         taxa_dir = os.listdir(args.newtaxa_dir)
+        new_taxa_dict =
         with open(args.dict_file) as json_data:
             read_dict = json_data.read()
             name_dict = json.loads(read_dict)
@@ -74,74 +75,10 @@ def main():
                     new_name_file.write('>')
                     new_name_file.write(replaces)
                     new_name_file.write('\n')
-
-
-
-
-
-
-        # # split on newlines for alignment data
-        # strp = readdata.strip()
-        # splt = strp.split('>')
-        #
-        # # drop wonky space at beginning of list of splits
-        # clean_splt = splt[1:]
-        #
-        # # initiate dictionary to contain old and new names
-        # newnames_oldnames = {}
-        # OTU_count = 0
-        #
-        # # iterate over sequences and add original names plus new names to dictionary
-        # # keys = new newnames
-        # # values = old names
-        # for i in clean_splt:
-        #     count = 0
-        #     OTU_count+=1
-        #     j = i.split('\n')
-        #     for segment in j:
-        #         count+=1
-        #         if count==1:
-        #             newnames_oldnames['OTU_' + str(OTU_count)] = segment
-        #
-        # # open file to output dictionary
-        # OTU_name_dict = "OTU_name_dict.txt"
-        # new_alignment = open("renamed_alignment.fa", 'w')
-        #
-        # # iterate through dictionary and list of sequences
-        # # replace old names with new names
-        # for key, value in newnames_oldnames.iteritems():
-        #     for item in clean_splt:
-        #         if re.match(value, item):
-        #             replaces = re.sub(value, key, item)
-        #             new_alignment.write(">")
-        #             new_alignment.write(replaces)
-        #             new_alignment.write('\n')
-        #
-        # # read tree file
-        # # topology = open(tree, 'r')
-        # # read_topo = topology.read()
-        # tree_name_count = 0
-        #
-        # # initialize str to be used as tree saving device outside of for loop
-        # swap = ''
-        # swap = re.sub(value, key, read_topo)
-        #
-        # # iterate over tree and replace old names with new names
-        # for key, value in newnames_oldnames.iteritems():
-        #     swap = re.sub(value, key, swap)
-        #
-        # # write new tree file
-        # new_tree = open('renamed_tree.tre','w')
-        # new_tree.write(swap)
-        #
-        # # write dictionary of names file
-        # with open(OTU_name_dict, 'w') as f:
-        #     for key, value in newnames_oldnames.items():
-        #         f.write('%s:%s\n' % (key, value))
-        #
-        #
-        # new_tree.close()
-        # new_alignment.close()
+        OTU_name_dict = "OTU_name_dict.txt"
+        # write dictionary of names file
+        with open(OTU_name_dict, 'w') as f:
+            json.dump(name_dict, f)
 
     else:
         alignment = open(args.alignment_file)

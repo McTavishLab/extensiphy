d968bc19c885bea54cd983863ed48f44f70ad39e
vortacs <jtoscanifield@ucmerced.edu>
2019-02-01 13:23:24 -0800

    removed some cd stuff

--- function_compare_test/method_compare.sh
+++ function_compare_test/method_compare.sh
@@ -9,18 +9,12 @@ set -o pipefail
 # get phycorder directory location for pathing
 PHYCORDER=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-compare_path = function_compare_test
-
-cd $PHYCORDER
-
-cd function_compare_test
-
 mkdir first_5_for_phycorder
 
-$PHYCORDER/taxon_splitter.py -m --max_num 5 --taxa_dir $PHYCORDER/function_compare_test/fastq_files --new_dir $PHYCORDER/function_compare_test/first_5_for_phycorder
+$PHYCORDER/../taxon_splitter.py -m --max_num 5 --taxa_dir $PHYCORDER/fastq_files --new_dir $PHYCORDER/first_5_for_phycorder
 
-$PHYCORDER/gon_phyling.sh $PHYCORDER/function_compare_test/gon_phy_first_5.cfg
+$PHYCORDER/../gon_phyling.sh $PHYCORDER/gon_phy_first_5.cfg
 
-$PHYCORDER/multi_map.sh $PHYCORDER/function_compare_test/phycorder_main_run.cfg
+$PHYCORDER/../multi_map.sh $PHYCORDER/phycorder_main_run.cfg
 
-$PHYCORDER/gon_phyling.sh $PHYCORDER/function_compare_test/gon_phy_main_run.cfg
+$PHYCORDER/../gon_phyling.sh $PHYCORDER/gon_phy_main_run.cfg

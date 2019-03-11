553f18136c62729b4eed26a6861ef79536744ac1
vortacs <jtoscanifield@ucmerced.edu>
2019-03-10 20:33:27 -0700

    working on joining all sequences for final output file

--- parallel_parsnp_splitter.py
+++ parallel_parsnp_splitter.py
@@ -6,7 +6,7 @@ import re
 
 print(sys.argv)
 
-infile = sys.argv[1]
+datafile = sys.argv[1]
 
 infile = open(sys.argv[1], 'r')
 header = []
@@ -26,6 +26,28 @@ prime_regex = string_to_convert1 + "(.*?)" + ">" + ending_num + ":"
 regex2 = re.compile(prime_regex, re.S)
 print(prime_regex)
 
-with open(infile) as fp:
+
+seq = []
+with open(datafile) as fp:
     for result in re.findall(regex2, fp.read()):
-        print(result)
+        seq.append(result)
+
+almost_clean_seq = []
+
+for item in seq:
+    num_reg = ":p\d+"
+    reg_compile = re.compile(num_reg)
+    string_search = re.findall(reg_compile, item)
+    if string_search:
+        # print(string_search)
+        new_seq = item.split(''.join(string_search))
+        almost_clean_seq.append(new_seq[1])
+
+newlines_left_seq = []
+for seq in almost_clean_seq:
+    new_seq = ''.join(seq).strip()
+    newlines_left_seq.append(new_seq)
+
+print(newlines_left_seq)
+# clean_seq = newlines_left_seq.strip("\n")
+# print(clean_seq)

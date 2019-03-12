750b6b560c240df4bb4647697f6d9f43938b7e72
vortacs <jtoscanifield@ucmerced.edu>
2019-03-11 19:58:50 -0700

    works but need a way to not print a "\n" at the start of the file

--- parallel_parsnp_splitter.py
+++ parallel_parsnp_splitter.py
@@ -8,23 +8,52 @@ print(sys.argv)
 
 datafile = sys.argv[1]
 
-infile = open(sys.argv[1], 'r')
+infile = open(datafile, 'r')
 header = []
 i=0
+name_count = 0
+seq_name_id = "##SequenceIndex " + sys.argv[2] + "\n"
+seq_name_id_grabber = re.compile(seq_name_id)
+
+taxa_count_regex = seq_name_id = "##SequenceIndex " + "\d+"
+taxa_count_compile = re.compile(taxa_count_regex)
+taxa_counter = 0
 for lin in infile:
     if lin.startswith('#'):
-        header.append(lin)
+        # header.append(lin)
+        count_taxa = re.search(taxa_count_compile, lin)
+        grab_taxa_name = re.search(seq_name_id_grabber, lin)
         i+=1
+        if count_taxa:
+            taxa_counter+=1
+
+        if grab_taxa_name:
+            name_count = i
+
+        if i == name_count + 1:
+            clean_name = lin.replace("##SequenceFile ", "")
+            header.append(clean_name)
 
 infile.close()
 
 string_to_convert1 = ">" + sys.argv[2] + ":"
 regex1 = re.compile(string_to_convert1)
 
-ending_num = str(int(sys.argv[2]) + 1)
-prime_regex = string_to_convert1 + "(.*?)" + ">" + ending_num + ":"
-regex2 = re.compile(prime_regex, re.S)
-print(prime_regex)
+if int(sys.argv[2]) == taxa_counter:
+    ending_num = str(1)
+    prime_regex = string_to_convert1 + "(.*?)" + ">" + ending_num + ":"
+    regex2 = re.compile(prime_regex, re.S)
+    print(prime_regex)
+
+elif int(sys.argv[2]) != taxa_counter:
+    ending_num = str(int(sys.argv[2]) + 1)
+    prime_regex = string_to_convert1 + "(.*?)" + ">" + ending_num + ":"
+    regex2 = re.compile(prime_regex, re.S)
+    print(prime_regex)
+
+else:
+    print("Problem with taxa counting")
+    exit
 
 
 seq = []
@@ -55,6 +84,15 @@ for seq in newlines_left_seq:
 
 clean_seq = ''.join(clean_seq)
 
-# new_seq_file = open("parsnp_chunk-" + sys.argv[2] + "-.fa",'w')
-# new_seq_file.write(clean_seq)
-# new_seq_file.close()
+#print(">" + header[1])
+#print(header[1])
+# print(clean_seq)
+
+
+
+new_seq_file = open("parsnp_chunk-" + sys.argv[2] + "-.fa",'w')
+new_seq_file.write("\n")
+new_seq_file.write(">")
+new_seq_file.write(header[1])
+new_seq_file.write(clean_seq)
+new_seq_file.close()

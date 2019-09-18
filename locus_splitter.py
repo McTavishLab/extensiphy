328238597c3b2bbeb461bc5ea0cbd6ec9ac5b9cd
vortacs <jtoscanifield@ucmerced.edu>
2019-09-18 13:39:24 -0700

    script outputs a split parsnp output file with seperate loci in each

--- locus_splitter.py
+++ locus_splitter.py
@@ -15,21 +15,36 @@ def parse_args():
 def main():
     args = parse_args()
     
-    taxa_count = 0
-    use_lines = False
+    
     full_sequences = {}
-    current_seq = ''
-    stored_name = ''
+    locus_chunk = ''
+    stored_names = []
+    stored_ids = []
     locus_id = "(>1:\d+-\d+\s+(\+|\-)\s" + args.locus_id + "\s(.|\n)+?)="
+    print(locus_id)
     locus_id_grabber = re.compile(locus_id, re.S)
-    #print(locus_id) 
+    file_name = "##SequenceFile(.*?)\n"
+    file_name_grabber = re.compile(file_name)
+    
     alignment = open(args.align_file, "r")
     read_align = alignment.read()
+    #lines = alignment.readlines()
     locus_search = re.search(locus_id_grabber, read_align)
     if locus_search:
-        print(locus_search.group())
-
-    
+        #print(locus_search.group())
+        locus_chunk = locus_search.group()
+    #print(locus_chunk)
+    name_search = re.findall(file_name_grabber, read_align)
+    for num, name in enumerate(name_search):
+        locus_num = ">" + str(num+1) + ":.*"
+        locus_num_grabber = re.compile(locus_num)
+        locus_chunk = re.sub(locus_num_grabber, ">" + name , locus_chunk)
+    locus_chunk = re.sub("=", "", locus_chunk)
+    locus_chunk = re.sub("> ", ">", locus_chunk)
+    locus_file = open(args.out_file, "w")
+    locus_file.write(locus_chunk)
+    locus_file.close()
+    alignment.close()
 
 
 

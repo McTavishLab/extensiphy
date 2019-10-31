8a72f7c88bf8ea38d7797f6cc386f7b73fda4fd7
vortacs <jtoscanifield@ucmerced.edu>
2019-10-31 16:19:35 -0700

    changed dict file to csv

--- new_locus_combiner.py
+++ new_locus_combiner.py
@@ -5,12 +5,13 @@ import argparse
 import re
 import json
 from collections import defaultdict
+import csv
 
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--msa_folder')
     parser.add_argument('--out_file')
-    parser.add_argument('--position_dict_file')
+    parser.add_argument('--position_csv_file')
     parser.add_argument('--suffix')
     return parser.parse_args()
 
@@ -19,16 +20,21 @@ def main():
     args = parse_args()
 
     msa_list = os.listdir(args.msa_folder)
-    print(msa_list)
+    #print(msa_list)
    
     name_list = []
     file_name_and_seq_len_dict = {}
     taxon_name_and_seqs = defaultdict(list)
     final_dict = {}
-
+    tuple_name_and_seq_len_list = []
     loci_count = 0
+    csv_header = ['locus_position_number', 'locus_file_name', 'locus_length']
+    tuple_name_and_seq_len_list.append(csv_header)
+
     for file_name in msa_list:
         if file_name.endswith(args.suffix):
+            locus_name = file_name
+            locus_len = 0
             loci_count+=1
             open_file = open(args.msa_folder + file_name, "r")
             read_file = open_file.read()
@@ -43,12 +49,25 @@ def main():
                     #print(seq_len)
                     taxon_name_and_seqs[name].append(seq)
                     file_name_and_seq_len_dict[file_name] = seq_len
+                    locus_len = seq_len
+                    #tuple_name_and_seq_len_list.append(name_and_len)
                     if file_name not in name_list:
                         name_list.append(file_name)
-
-
+            name_and_len = [loci_count, file_name, seq_len]
+            tuple_name_and_seq_len_list.append(name_and_len)
+    print(tuple_name_and_seq_len_list)
     #print(file_name_and_seq_len_dict)
     #print(taxon_name_and_seqs)
+    
+    myFile = open(args.position_csv_file, 'w')
+    with myFile:
+        writer = csv.writer(myFile)
+        writer.writerows(tuple_name_and_seq_len_list)
+     
+    print("Writing complete") 
+    
+    
+    
     
     for num, f_name in enumerate(name_list):
         if f_name in file_name_and_seq_len_dict.keys():
@@ -57,8 +76,8 @@ def main():
             final_dict[num] = temp_dict
             #final_dict[num] = "{" + str(file_name_and_seq_len_dict[f_name]) + ":" + f_name + "}"
 
-    print("FINAL DICT OF POSITIONS")
-    print(final_dict)
+    #print("FINAL DICT OF POSITIONS")
+    #print(final_dict)
 
     msa_file = open(args.out_file,'w')
     for name, seqs in taxon_name_and_seqs.items():
@@ -80,8 +99,8 @@ def main():
 
 
 
-    with open(args.position_dict_file, 'w') as dict_output:
-        json.dump(final_dict, dict_output)
+    #with open(args.position_dict_file, 'w') as dict_output:
+    #    json.dump(final_dict, dict_output)
 #    concat_file.close()
 
 

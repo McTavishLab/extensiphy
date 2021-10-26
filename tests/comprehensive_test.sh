f3315bd8b1b8b8fb0569eedb24673602515c0185
vortacs <jtoscanifield@ucmerced.edu>
2021-10-26 15:36:10 -0700

    first run basic test done

--- tests/comprehensive_test.sh
+++ tests/comprehensive_test.sh
@@ -2,6 +2,30 @@
 # Comprehensive test of Extensiphy options and outputs
 # establishes the path to find the phycorder directory
 
+set -e
+#-e Exit immediately if a pipeline (see Pipelines), which may consist of a
+#single simple command (see Simple Commands), a list (see Lists), or a compound
+# command (see Compound Commands) returns a non-zero status. The shell does not
+# exit if the command that fails is part of the command list immediately
+# following a while or until keyword, part of the test in an if statement,
+# part of any command executed in a && or || list except the command following
+# the final && or ||, any command in a pipeline but the last, or if the
+#command’s return status is being inverted with !. If a compound command other
+#than a subshell returns a non-zero status because a command failed while -e was
+# being ignored, the shell does not exit. A trap on ERR, if set,
+#is executed before the shell exits.
+
+set -u
+#-u Treat unset variables and parameters other than the special parameters
+# ‘@’ or ‘*’ as an error when performing parameter expansion.
+#An error message will be written to the standard error, and
+#a non-interactive shell will exit.
+
+set -o pipefail
+# -o pipefail If set, the return value of a pipeline is the value of the last
+#(rightmost) command to exit with a non-zero status, or zero if all commands in
+#the pipeline exit successfully. This option is disabled by default.
+
 
 touch test_results.txt
 
@@ -9,18 +33,16 @@ touch test_results.txt
 ## Question do we want the help mneu to crash if programs not installed?
 
 # Check that some expected text is there
-VAR=$(grep 'Extensiphy is a program for quickly adding genomic sequence' test_help.txt | wc)
+VAR=$(grep 'Extensiphy is a program for quickly adding genomic sequence' test_help.txt | wc -l)
 
-if [[ $(echo $VAR | cut -f1 -d' ') -eq 1 ]]
+if [[ $(echo $VAR) -eq 1 ]]
 then
   echo "text found in help output."
-  echo "test help test passed" > test_results.txt
+  echo "test help menu: PASSED" > test_results.txt
 else
-  echo "NOOOOOOOO"
-  echo "test help test FAILED" > test_results.txt
+  echo "text found in help output"
+  echo "test help menu: FAILED" > test_results.txt
 fi
-
-
 
 
 touch logfile.txt
@@ -29,10 +51,25 @@ touch logfile.txt
 # Examine: extended.aln
 ../extensiphy.sh -u ALIGN -a ../testdata/combo.fas -d ../testdata -1 _R1.fq -2 _R2.fq -o first_extensiphy_run >> logfile.txt 2>&1
 
+num_seqs=$(grep -c ">" ./first_extensiphy_run/RESULTS/extended.aln)
+num_lines=$(wc -l ./first_extensiphy_run/RESULTS/extended.aln)
+num_chars=$(wc ./first_extensiphy_run/RESULTS/extended.aln | | cut -f11 -d ' ')
+
+if [[ $num_seqs != 23 ]]
+then
+  echo "text found in help output."
+  echo "test simple alignment update: PASSED" > test_results.txt
+else
+  echo "text found in help output"
+  echo "test simple alignment update: FAILED" > test_results.txt
+fi
+
 # Test that EP runs and produces and alignment and phylogeny
 # Tests flags: -a, -d, -1, -2 , -u PHYLO, -o
 # Examine: extended.aln, RAxML_bestTree.consensusFULL
 ../extensiphy.sh -u PHYLO -a ../testdata/combo.fas -d ../testdata -1 _R1.fq -2 _R2.fq -o second_extensiphy_run >> logfile.txt 2>&1
+
+
 
 # Test that EP updates an existing phylogeny
 # Tests flags: -a, -d, -1, -2, -u PHYLO, -t, -o
